7a80dbca43305efd12a7e13035d861018e0d7e7d
Radostin Handarov <frochbg@gmail.com>
2023-03-08 11:02:46 +0200

    Added header for VendingMachineCollection class

---
+++ Task 1/Task 2/VendingMachineCollection.hpp
@@ -0,0 +1,36 @@
+#ifndef VENDING_MACHINE_COLLECTION_H
+#define VENDING_MACHINE_COLLECTION_H
+
+#include "VendingMachine.hpp"
+
+class VendingMachineCollection {
+private:
+	VendingMachine* machines;
+	unsigned int numNachines;
+	unsigned int capacity;
+public:
+	VendingMachineCollection();
+	VendingMachineCollection(const VendingMachineCollection& other);
+
+	VendingMachineCollection& operator=(const VendingMachineCollection& other);
+
+	void addMachine();
+
+	//returns the address in the heap
+	const char* getMachineAddressById(unsigned int id) const;
+	void getNumGoodsInMachineById(unsigned int id, unsigned int& numSweets,
+		unsigned int& numSnacks, unsigned int& numBeverages) const;
+
+	void sellSweet(unsigned int machineId);
+	void sellSnack(unsigned int machineId);
+	void sellBeverage(unsigned int machineId);
+
+	void addSweet(unsigned int machineId, unsigned int amount);
+	void addSnack(unsigned int machineId, unsigned int amount);
+	void addBeverage(unsigned int machineId, unsigned int amount);
+
+	~VendingMachineCollection();
+};
+
+#endif // !VENDING_MACHINE_COLLECTION_H
+
